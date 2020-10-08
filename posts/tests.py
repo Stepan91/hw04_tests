bd8fc405bb7aa29dfb1e8d76d936d2d3c8b1abf1
Stepan Pryn <prynstepan@yandex.ru>
2020-10-09 00:38:32 +0300

    hw after correction

--- posts/tests.py
+++ posts/tests.py
@@ -1,24 +1,19 @@
 from django.test import TestCase
 from django.test import Client
-from .models import User, Post
+from .models import User, Post, Group
 from . import views
 from django.urls import reverse
 
-# Create your tests here.
-
-
-class TestStringMethods(TestCase):
-    def test_length(self):
-        self.assertEqual(len('yatube'), 6)
-    
-    def test_show_msg(self):
-        self.assertTrue(True, msg='Важная проверка на истинность')
-
 
 class ScriptsTest(TestCase):
+    urls = {
+        'INDEX': reverse('index'),
+        'LOGIN': reverse('login'),
+        'NEW_POST': reverse('new_post'),
+        }
+    
     def setUp(self):
         self.client = Client()
-        # регистрация нового пользователя
         self.user = User.objects.create_user(
                         first_name = 'sarah',
                         last_name = 'connor',
@@ -26,84 +21,70 @@ class ScriptsTest(TestCase):
                         email='connor.s@skynet.com', 
                         password='12345'
                         )
-        # содание поста новым пользователем
-        self.post = Post.objects.create(text='GOGOGOGOGO', author=self.user)
+        self.group = Group.objects.create(title='test_group', slug='test')
+        self.urls['REDIRECT'] = (f'{self.urls["LOGIN"]}?next='
+                                 f'{self.urls["NEW_POST"]}')
+        self.urls['PROFILE'] = reverse(
+                                   'profile', 
+                                   kwargs={'username': 'sarah77'}
+                                )
 
+
+    # redirect_URL = reverse('profile', kwargs={'username': 'sarah77'})
+ 
 
     def test_profile(self):
-        # формирмирование GET-запроса к странице профиля нового пользователя
         response = self.client.get('/sarah77/')
-        # проверка, что страница найдена
         self.assertEqual(response.status_code, 200)
-        # проверка, что новый пользователь - экземпляр класса User
         self.assertIsInstance(response.context['author'], User)
-        # проверка имени нового пользователя
         self.assertEqual(response.context['author'].username, self.user.username)
     
 
     def test_post_autorized(self):
-        response = self.client.get('/sarah77/')
-        # проверка создания поста новым авторизованным пользователем
-        self.assertContains(response, 'GOGOGOGOGO', status_code=200)
+        self.client.force_login(self.user)
+        response = self.client.get(self.urls['NEW_POST'])
+        self.assertEqual(response.status_code, 200)
 
 
     def test_post_NotAutorized(self):
         response = self.client.get('/new/')
-        # проверка невозможности создания поста неавторизованным пользователем
-        self.assertRedirects(response, '/auth/login/?next=/new/')
+        self.assertRedirects(response, self.urls['REDIRECT'])
 
 
-    def test_create_post_index(self):
+    def test_post_on_pages(self):
+        self.client.force_login(self.user)
+        self.post = Post.objects.create(text='GOGOGOGOGO', author=self.user)
+        
         response = self.client.get('/')
-        # проверка отображения поста на главной странице
-        self.assertContains(response, self.post.text, status_code=200)
+        self.assertContains(response, self.post.text, status_code=200) 
 
-
-    def test_create_post_post(self):
         response = self.client.post(
-                        reverse(
-                            'post', 
-                            kwargs={'username': 'sarah77', 'post_id': self.post.id}
-                            )
+                       reverse(
+                           'post', 
+                           kwargs={
+                               'username': 'sarah77', 'post_id': self.post.id
+                            }
                         )
-        # проверка отображения поста на странице поста
+                    )
         self.assertContains(response, self.post.text, status_code=200)
     
-
-    def test_create_post_profile(self):
-        response = self.client.post(
-                            reverse(
-                            'profile', 
-                            kwargs={'username': 'sarah77'}
-                            )
-                        )
-        # проверка отображения поста на странице профиля пользователя
+        response = self.client.post(self.urls['PROFILE'])
         self.assertContains(response, self.post.text, status_code=200)
     
     def test_edit_post(self):
+        self.client.force_login(self.user)
+        self.post = Post.objects.create(text='GOGOGOGOGO', author=self.user)
+        new_data = {
+            'text': 'new_text_for_post',
+            'group': self.group.id
+            }
         response = self.client.post(
-                            reverse(
-                            'post_edit', 
-                            kwargs={'username': 'sarah77', 'post_id': self.post.id},
-                            ),
-                            follow=True
-                        )
-        # авторизованный пользователь может редактировать пост
-        self.assertContains(response, 'GOGOGOGOGO', status_code=200)
-        # проверка наличия измененного поста на всех связанных страницах
-        self.assertTrue(self.test_create_post_index)
-        self.assertTrue(self.test_create_post_post)
-        self.assertTrue(self.test_create_post_profile)
-        
-
-
-
-
-
-
-
-
-
-    
-
-        
+                        reverse(
+                        'post_edit', 
+                        kwargs={'username': self.user.username, 'post_id': self.post.id}
+                        ),
+                        new_data,
+                        follow=True
+                    )
+        self.assertContains(response, new_data['text'], status_code=200)
+        self.assertTrue(self.test_post_on_pages)
--- posts/views.py
+++ posts/views.py
@@ -21,7 +21,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = Post.objects.all().filter(group=group)
+    post_list = group.posts.filter(group=group)
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
@@ -34,21 +34,18 @@ def group_posts(request, slug):
 
 @login_required
 def new_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('index')
-        return render(request, 'new.html', {'form': form})
-    form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('index')
     return render(request, 'new.html', {'form': form})
     
 
 def profile(request, username):
-    author = User.objects.get(username=username)
-    post_list = Post.objects.filter(author=author)
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.filter(author=author)
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
@@ -60,29 +57,30 @@ def profile(request, username):
 
 
 def post_view(request, username, post_id):
-        user = get_object_or_404(User, username=username)
-        post = get_object_or_404(Post, author=user, id=post_id)
-        return render(
-            request,
-            'post.html',
-            { 'post': post, 'author': user }
-            )
+    user = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, id=post_id)
+    return render(
+        request,
+        'post.html',
+        { 'post': post, 'author': user }
+        )
 
 
 def post_edit(request, username, post_id):
-        user = get_object_or_404(User, username=username)
-        login_user = request.user
-        post = get_object_or_404(Post, author=user, id=post_id)
-        if login_user != user:
-            return redirect('post', username=user, post_id=post_id)
-        form = PostForm()
-        if request.method == 'POST':
-            form = PostForm(request.POST)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.save()
-                return redirect('post', username=user, post_id=post_id)
-            return render(request, 'new.html', {'form': form, 'post': post})
-        form = PostForm(instance=post)
-        return render(request, 'new.html', {'form': form, 'post': post})
+    # достать пользователя из post (строка 79) смог только 
+    # после объявления переменной post.
+    # Без переменной user не получилось обойтись.
+    user = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, author=user, id=post_id)
+    edit_flag = True
+    if request.user != post.author:
+        return redirect('post', username=post.author, post_id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        post = form.save()
+        return redirect('post', username=request.user.username, post_id=post_id)
+    return render(
+        request, 
+        'new.html', 
+        {'form': form, 'post': post, 'edit_flag': edit_flag,}
+        )
